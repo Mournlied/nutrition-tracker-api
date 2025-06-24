fa5aa3479479be8a4c3d7be74cbb1e0987bbe6e9
Mournlied <63529259+Mournlied@users.noreply.github.com>
2025-06-23 23:22:46 -0400

    arregla rolId tipo de dato incorrecto de Long a Integer.

--- src/main/java/com/mournlied/nutrition_tracker_api/repository/RolRepository.java
+++ src/main/java/com/mournlied/nutrition_tracker_api/repository/RolRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RolRepository extends JpaRepository<Rol,Long> {
+public interface RolRepository extends JpaRepository<Rol,Integer> {
     @Query("""
     SELECT DISTINCT r FROM Rol r
     LEFT JOIN FETCH r.permisos
