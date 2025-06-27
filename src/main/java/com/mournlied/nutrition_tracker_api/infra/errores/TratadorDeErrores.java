7e90f40d0e66fae02aa94a4a13b747ebca5154ed
Mournlied <63529259+Mournlied@users.noreply.github.com>
2025-06-26 21:41:08 -0400

    refactoriza nombres de metodos en TradadorDeErrores.  cambia a nombre de error + codigos HTTP correspondiente.

--- src/main/java/com/mournlied/nutrition_tracker_api/infra/errores/TratadorDeErrores.java
+++ src/main/java/com/mournlied/nutrition_tracker_api/infra/errores/TratadorDeErrores.java
@@ -17,53 +17,53 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class TratadorDeErrores {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> tratarError404(EntityNotFoundException e){
+    public ResponseEntity<String> tratarEntidadNoEncontrada404(EntityNotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<String> tratarError403(AccessDeniedException e) {
+    public ResponseEntity<String> tratarAccesoDenegado403(AccessDeniedException e) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
     }
 
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<String> tratarError401(AuthenticationException e) {
+    public ResponseEntity<String> tratarNoAutorizado401(AuthenticationException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
     @ExceptionHandler(ObjetoRequeridoNoEncontrado.class)
-    public ResponseEntity<String> tratarError400ObjetoRequeridoNoEncontrado(ObjetoRequeridoNoEncontrado e) {
+    public ResponseEntity<String> tratarObjetoRequeridoNoEncontrado400(ObjetoRequeridoNoEncontrado e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> tratarNullBody(HttpMessageNotReadableException e) {
+    public ResponseEntity<String> tratarNullBody400(HttpMessageNotReadableException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Required request body is missing");
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> tratarError400(MethodArgumentNotValidException e){
+    public ResponseEntity<?> tratarArgumentoInvalido400(MethodArgumentNotValidException e){
         var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errores);
     }
 
     @ExceptionHandler(SecurityException.class)
-    public ResponseEntity<String> tratarSecurityException(SecurityException e) {
+    public ResponseEntity<String> tratarExcepcionDeSeguridad400(SecurityException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<String> tratarErrorValidacionesDeNegocio(ValidationException e){
+    public ResponseEntity<String> tratarValidacionesDeNegocio400(ValidationException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(ValidacionDeIntegridad.class)
-    public ResponseEntity<String> tratarErrorValidacionesIntegridad(ValidacionDeIntegridad e){
+    public ResponseEntity<String> tratarValidacionesIntegridad409(ValidacionDeIntegridad e){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<String> tratarErrorDeServidor(IllegalStateException e) {
+    public ResponseEntity<String> tratarErrorDeServidor500(IllegalStateException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 
