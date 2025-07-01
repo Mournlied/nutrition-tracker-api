a0658c8c8180ba210e3609459951094ddd01caff
Mournlied <63529259+Mournlied@users.noreply.github.com>
2025-06-30 21:56:55 -0400

    agrega bean JwtDecoder faltante

--- src/main/java/com/mournlied/nutrition_tracker_api/infra/security/SecurityConfig.java
+++ src/main/java/com/mournlied/nutrition_tracker_api/infra/security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.mournlied.nutrition_tracker_api.infra.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -31,6 +34,12 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(
+            @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUri) {
+        return JwtDecoders.fromIssuerLocation(issuerUri);
     }
 
     @Bean
