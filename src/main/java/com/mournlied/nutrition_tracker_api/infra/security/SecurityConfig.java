8ecc52d97c374441c87e640710117ef31d931e75
Mournlied <63529259+Mournlied@users.noreply.github.com>
2025-07-02 21:38:39 -0400

    agrega anotacion @Profile para asegurar uso de SecurityTestConfig.class en tests.

--- src/main/java/com/mournlied/nutrition_tracker_api/infra/security/SecurityConfig.java
+++ src/main/java/com/mournlied/nutrition_tracker_api/infra/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.mournlied.nutrition_tracker_api.infra.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
+@Profile("!test")
 public class SecurityConfig {
 
     private final CustomJwtRoleAndPermissionConverter customConverter;
