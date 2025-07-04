d1503d02a7dd97037e8d76989d25bdf916b9dc5f
Mournlied <63529259+Mournlied@users.noreply.github.com>
2025-07-03 22:06:57 -0400

    agrega info personal para test user 1

--- src/test/resources/db/migration/V9001__seed-test-user.sql
+++ src/test/resources/db/migration/V9001__seed-test-user.sql
@@ -6,3 +6,14 @@ SELECT
     CURRENT_DATE
 FROM roles r
 WHERE r.nombre_rol = 'USER';
+
+INSERT INTO informacion_personal(user_id, peso_inicial,nombre,nacimiento,altura,objetivos)
+SELECT
+    u.user_id,
+    0,
+    '',
+    CURRENT_DATE,
+    0,
+    ''
+FROM users u
+WHERE u.correo = 'user1@mournlied.com';
