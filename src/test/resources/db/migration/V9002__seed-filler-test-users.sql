15b8ec5c154b312114f41b4dce175d111d262cc5
Mournlied <63529259+Mournlied@users.noreply.github.com>
2025-07-03 22:08:22 -0400

    agrega migracion de test. inserta multiples users normales y admins para tests.

---
+++ src/test/resources/db/migration/V9002__seed-filler-test-users.sql
@@ -0,0 +1,23 @@
+INSERT INTO users (correo, rol_id, estado, user_creacion)
+SELECT
+    'user' || x || '@mournlied.com',
+    r.rol_id,
+    1,
+    CURRENT_DATE
+FROM
+    generate_series(1, 9) x,
+    roles r
+WHERE
+    r.nombre_rol = 'USER';
+
+INSERT INTO users (correo, rol_id, estado, user_creacion)
+SELECT
+    'admin' || y || '@mournlied.com',
+    r.rol_id,
+    1,
+    CURRENT_DATE
+FROM
+    generate_series(2, 5) y,
+    roles r
+WHERE
+    r.nombre_rol = 'ADMIN';
